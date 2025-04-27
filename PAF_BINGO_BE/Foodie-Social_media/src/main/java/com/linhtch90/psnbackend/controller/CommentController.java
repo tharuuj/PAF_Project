6945c69ee3e64c661c8764c2efc078434b40b722
Poornisl <poornithilakarathna21@gmail.com>
2025-04-27 09:35:52 +0530

    update comment controller

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/CommentController.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.linhtch90.psnbackend.controller;
 
+// Import required classes
 import com.linhtch90.psnbackend.entity.CommentEntity;
 import com.linhtch90.psnbackend.entity.CommentPostRequestEntity;
 import com.linhtch90.psnbackend.entity.IdObjectEntity;
@@ -12,11 +13,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Define this class as a REST controller and set base URL as /api/v1
 @RestController
 @RequestMapping("/api/v1")
 public class CommentController {
+
+    // Inject CommentService to use its methods
     @Autowired
     private CommentService commentService;
+
+    // API to insert a new comment for a post
     @PostMapping("/insertcomment")
     public ResponseEntity<ResponseObjectService> insertComment(@RequestBody CommentPostRequestEntity postedComment) {
         CommentEntity inputComment = postedComment.getCommentEntity();
@@ -24,30 +30,33 @@ public class CommentController {
         return new ResponseEntity<ResponseObjectService>(commentService.insertComment(inputComment, inputPostId.getId()), HttpStatus.OK);
     }
 
+    // API to get all comments for a specific post
     @PostMapping("/getcomments") 
     public ResponseEntity<ResponseObjectService> getComments(@RequestBody IdObjectEntity inputPostId) {
         return new ResponseEntity<ResponseObjectService>(commentService.getComments(inputPostId.getId()), HttpStatus.OK);
     }
 
+    // API to get all comments across all posts
     @PostMapping("/getallcomments")
     public ResponseEntity<ResponseObjectService> getAllComments() {
         return new ResponseEntity<ResponseObjectService>(commentService.getAllComments(), HttpStatus.OK);
     }
+
+    // API to edit an existing comment for a post
     @PutMapping("/editcomment")
     public ResponseEntity<ResponseObjectService> putComment(@RequestBody CommentPostRequestEntity postedComment) {
-        System.out.println(postedComment);
+        System.out.println(postedComment); // Log the incoming request
         CommentEntity inputComment = postedComment.getCommentEntity();
         IdObjectEntity inputPostId = postedComment.getPostId();
         return new ResponseEntity<ResponseObjectService>(commentService.editUserComment(inputComment, inputPostId.getId()), HttpStatus.OK);
-
-//        return new ResponseEntity<ResponseObjectService>(commentService.editUserComment(inputPost), HttpStatus.OK);
     }
+
+    // API to delete a comment from a post
     @PutMapping("/deletecomment")
     public ResponseEntity<ResponseObjectService> deletePost(@RequestBody CommentPostRequestEntity postedComment) {
-        System.out.println(postedComment);
+        System.out.println(postedComment); // Log the incoming request
         CommentEntity inputComment = postedComment.getCommentEntity();
         IdObjectEntity inputPostId = postedComment.getPostId();
-
         return new ResponseEntity<ResponseObjectService>(commentService.deleteUserComment(inputComment, inputPostId.getId()), HttpStatus.OK);
     }
 
