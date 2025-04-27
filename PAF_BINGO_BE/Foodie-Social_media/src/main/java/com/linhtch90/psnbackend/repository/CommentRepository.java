76d4821ae03d746cab6ad750a4458185640a003e
Poornisl <poornithilakarathna21@gmail.com>
2025-04-27 09:44:40 +0530

    Create CommentRepository interface for CommentEntity CRUD operations

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/repository/CommentRepository.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/repository/CommentRepository.java
@@ -1,11 +1,14 @@
 package com.linhtch90.psnbackend.repository;
 
+// Import necessary classes
 import com.linhtch90.psnbackend.entity.CommentEntity;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+// Mark this interface as a Spring Repository (for MongoDB)
 @Repository
 public interface CommentRepository extends MongoRepository<CommentEntity, String> {
-    
+    // Inherits basic CRUD operations from MongoRepository
+    // CommentEntity is the type of document
+    // String is the type of the ID field
 }
