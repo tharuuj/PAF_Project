c8a7c9f9dddd5872e141d57d34c44f42df769ea0
Poornisl <poornithilakarathna21@gmail.com>
2025-04-27 09:42:37 +0530

    Create CommentPostRequestEntity class to handle comment and post ID request payload

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/entity/CommentPostRequestEntity.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/entity/CommentPostRequestEntity.java
@@ -1,13 +1,19 @@
 package com.linhtch90.psnbackend.entity;
 
+// Import Lombok annotations
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Lombok generates getters, setters, constructors, and more
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentPostRequestEntity {
+    
+    // The comment details (content, user, timestamp, etc.)
     private CommentEntity commentEntity;
+    
+    // The ID of the post that the comment belongs to
     private IdObjectEntity postId;
 }
