ee318542e2c7b3208b6d26e038cfc8ddded6f2e4
IT22628404 <it22628404@my.sliit.lk>
2025-05-02 20:55:20 +0530

    extract hashtags

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/repository/PostRepository.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/repository/PostRepository.java
@@ -7,6 +7,7 @@ import com.linhtch90.psnbackend.entity.PostEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PostRepository extends MongoRepository<PostEntity, String> {
     Optional<List<PostEntity>> findByUserId(String id);
