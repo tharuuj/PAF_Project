d3f3d23a2d09137877b7b52f9e4cd4d4f3448240
IT22628404 <it22628404@my.sliit.lk>
2025-05-02 20:38:27 +0530

    Add PostByFollowing

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/entity/PostByFollowing.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/entity/PostByFollowing.java
@@ -7,7 +7,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+
+//Add PostByFollowing
+
 public class PostByFollowing {
     private UserEntity user;
     private PostEntity post;
 }
+
